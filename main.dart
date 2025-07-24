aff4c0d7c8bd102ca4eb0642a8b51261d36ad964
danielfigueroa1879 <90109125+danielfigueroa1879@users.noreply.github.com>
2025-07-24 19:41:59 -0400

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,36 @@
+import 'package:bitacora_carabineros/screens/nuevo_procedimiento_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+// Importa tu archivo de configuración de Firebase (firebase_options.dart)
+// import 'firebase_options.dart';
+
+void main() async {
+  // Asegúrate de que los widgets de Flutter estén inicializados
+  WidgetsFlutterBinding.ensureInitialized();
+  // Inicializa Firebase (descomenta cuando tengas tu configuración)
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bitácora de Procedimientos',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.green.shade600,
+        scaffoldBackgroundColor: Colors.grey[900],
+        colorScheme: ColorScheme.dark(
+            primary: Colors.green.shade400, secondary: Colors.teal.shade300),
+      ),
+      home: NuevoProcedimientoScreen(),
+    );
+  }
+}
